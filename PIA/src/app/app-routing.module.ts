683a4036d7a9ba2a919ea14d0a121ae934a9462b
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2023-11-11 08:58:37 -0600

    Avance a la pagina de profile

--- PIA/src/app/app-routing.module.ts
+++ PIA/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'perfil',
+    component: ProfileComponent
   }
 
 ];
