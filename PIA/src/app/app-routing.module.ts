174e5e4187189aa44a125217676f57313b4913c6
Aldo <aldocmtz95@gmail.com>
2023-11-12 20:11:06 -0600

    avance feed

--- PIA/src/app/app-routing.module.ts
+++ PIA/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: FeedComponent
+  },{
+    path: 'feed',
+    component: FeedComponent
   }
 
 ];
--- PIA/src/app/app.module.ts
+++ PIA/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, FeedComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
