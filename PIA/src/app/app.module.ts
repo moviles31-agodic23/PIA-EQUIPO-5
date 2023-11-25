cdeb0dca9b28c446fe5108e47a4c059b3c44e188
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2023-11-24 23:37:35 -0600

    Diseño tentativo

--- PIA/src/app/app.module.ts
+++ PIA/src/app/app.module.ts
@@ -8,10 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, ProfileComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- PIA/src/app/profile/profile.component.ts
+++ PIA/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@angular/fire/storage'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from 'src/firebase';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent  implements OnInit {
+  src:any=[];
+  userInfo:any={
+    "bio": "",
+    "followers":NaN,
+    "nameUser": "",
+    "following": NaN,
+    "pfp": "",
+    "name": ""
+};
+  // private storage:Storage
+  constructor(private storage:Storage) { }
+  async getDocuments(){
+    try{const docRef= collection(db, 'posts');
+    const docSnap = await getDocs(docRef);
+    docSnap.forEach((doc)=>
+    {
+      
+      this.src.push(doc.data())
+    }
+    )
+    
+    
+  }catch(error){
+    console.log(error)
+  }
+    
+  }
+  async getUserInfo(){
+    try{
+      const docRef= doc(db, 'users','profile');
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap.data())
+      this.userInfo = docSnap.data()
+    
+  }catch(error){
+    console.log(error)
+  }
+    
+  }
 
-  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getUserInfo();
+    this.getDocuments()
+  }
 
 }
